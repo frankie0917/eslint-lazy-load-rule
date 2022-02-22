a1696fba9d004618dcde23315ef9da9b4f93ac40
frankie0917 <frankie0917@foxmail.com>
2022-02-22 13:29:39 +0800

    First commit

---
+++ docs/rules/lazy-load-rule.md
@@ -0,0 +1,32 @@
+# Eslint lazy loading rule
+
+Gives warning when lazy loading is possible (lazy-load-rule)
+
+When load time is important factor of your frontend application,
+you can use this rule to warn you whenever lazy loading is possible.
+
+## Rule Details
+
+This rule aims to help eliminate any lazy-loadable code that is not lazy-loaded.
+
+Examples of **incorrect** code for this rule:
+
+```js
+import {foo} from 'foo';
+if(true) foo();
+```
+
+Examples of **correct** code for this rule:
+Currently allows to use in logical statement or conditional statement;
+
+```js
+import {foo} from 'foo';
+true || foo();
+true && foo();
+true ? foo() : '';
+true ? '' : foo();
+```
+
+## Further Reading
+
+Best to use with `max-line` and `max-line-per-function` for reminding youself to put excess code in different module.
---
+++ tests/lib/rules/lazy-load-rule.js
@@ -0,0 +1,86 @@
+/**
+ * @fileoverview Gives warning when lazy loading is possible but not used
+ * @author frank-tomato
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/lazy-load-rule'),
+  RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2015,
+  },
+});
+
+const testCases = [
+  {
+    name: 'should not report when idetifier is not a imported identifier',
+    valid: [
+      {
+        code: `
+    const a = ()=>{};
+    true && a();
+    `,
+      },
+    ],
+  },
+  {
+    name: 'should not report when idetifier is whthin if test statement',
+    valid: [
+      {
+        code: `
+        import { foo } from './foo';
+        if (foo > 8 && 6 < 'foo') foo();
+        `,
+      },
+    ],
+  },
+  {
+    name: 'should not report when idetifier is whthin conditional statement',
+    valid: [
+      {
+        code: `
+        import { foo } from './foo';
+        true ? foo() : false;
+        `,
+      },
+    ],
+  },
+  {
+    name: 'should not report when idetifier is whthin logical statement',
+    valid: [
+      {
+        code: `
+        import { foo } from './foo';
+        foo && foo();
+        `,
+      },
+    ],
+  },
+  {
+    name: 'should report when idetifier is whthin if statement',
+    invalid: [
+      {
+        code: `
+        import {a} from 'a';
+        if(true) a();
+        `,
+        errors: [{ message: 'Consider using lazy loading' }],
+      },
+    ],
+  },
+];
+
+testCases.forEach((c) => {
+  ruleTester.run(c.name, rule, { invalid: [], valid: [], ...c });
+});
